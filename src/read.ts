ab78a388e434f9e323ee02dd0f9b3caabec973a0
Adam Pelc <sikulin94@gmail.com>
2024-04-21 19:04:22 +0200

    stepper for multiple arrays

--- src/read.ts
+++ src/read.ts
@@ -4,38 +4,73 @@
  * @param fn Function that is triggered for every value.
  */
 export const recurseObjects = (
-    obj: object,
-    fn: (key: string | number, val: unknown, obj: object) => void
-  ) => {
-    if (Array.isArray(obj)) {
-      obj.forEach((item, index) => {
-        fn(index, item, obj)
-  
-        if (typeof item === "object" && item !== null) {
-          recurseObjects(item, fn)
-        }
-      })
-    } else if (typeof obj === "object" && obj !== null) {
-      Object.entries(obj).forEach(([key, val]) => {
-        fn(key, val, obj)
-  
-        if (typeof val === "object" && val !== null) {
-          recurseObjects(val, fn)
-        }
-      })
+  obj: object,
+  fn: (key: string | number, val: unknown, obj: object) => void
+) => {
+  if (Array.isArray(obj)) {
+    obj.forEach((item, index) => {
+      fn(index, item, obj)
+
+      if (typeof item === "object" && item !== null) {
+        recurseObjects(item, fn)
+      }
+    })
+  } else if (typeof obj === "object" && obj !== null) {
+    Object.entries(obj).forEach(([key, val]) => {
+      fn(key, val, obj)
+
+      if (typeof val === "object" && val !== null) {
+        recurseObjects(val, fn)
+      }
+    })
+  }
+}
+
+/**
+ * @description Function iterates passed array and triggers function related to given step interval at appropriate index.
+ * @param arr An array of values.
+ * @param steps Tuple with first element being step after which function passed as second element would be triggered.
+ * @param start First index from which iteration starts.
+ */
+export const stepArray = <T>(
+  arr: T[],
+  steps: [number, (value: T, index: number, step: number) => void][],
+  start = 0
+) => {
+  for (let i = start; i < arr.length; i++) {
+    for (const [step, fn] of steps) {
+      if (i % step === 0) fn(arr[i], i, step)
     }
   }
-
-  /**
-   * @description Function iterates passed array and triggers function related to given step interval at appropriate index.
-   * @param arr An array of values.
-   * @param steps Tuple with first element being step after which function passed as second element would be triggered.
-   * @param start First index from which iteration starts.
-   */
-export const handleSteps = <T>(arr: T[], steps: [number, (value: T, index: number, step: number) => void][], start = 0) => {
-    for (let i = start; i < arr.length; i++) {
-        for (const [step, fn] of steps) {
-            if (i % step === 0) fn(arr[i], i, step)
-        }
-    }
 }
+
+/**
+ * @description Function iterates passed arrays and triggers function related to given step interval at appropriate index.
+ * @param arrs An array of arrays with values.
+ * @param steps Tuple with first element being step after which function passed as second element would be triggered.
+ * @param start First index from which iteration starts.
+ */
+export const stepArrays = <T extends unknown[]>(
+  arrs: T[],
+  steps: [number, (values: T[number][], index: number, step: number) => void][],
+  start = 0
+) => {
+  const longestArrLength = arrs.reduce(
+    (length, arr) => (arr.length > length ? arr.length : length),
+    0
+  )
+
+  for (let i = start; i < longestArrLength; i++) {
+    let values: T[number][] | undefined
+    
+    for (const [step, fn] of steps) {
+      if (i % step === 0) {
+        values ?? (values = arrs.map(arr => arr[i]))
+        
+        fn(values, i, step)
+      }
+    }
+  }
+}
+
+stepArrays([["a", { a: 1 }], [1]], [[2, (x) => {}]])
