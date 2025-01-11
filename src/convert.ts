d20d83ae466d848876d91914226bca7155b7d82b
Adam Pelc <sikulin94@gmail.com>
2025-01-11 08:52:10 +0100

    convert: round func.

---
+++ src/convert.ts
@@ -0,0 +1,17 @@
+export const round = (num: number, maxPrecision = 0): number => {
+    let str = num.toFixed(maxPrecision)
+    const isFloat = str.includes(".")
+
+    if (isFloat) {
+        for (let i = str.length - 1; i > 0; i--) {
+            const char = str[i]
+
+            if (char !== "0") {
+                str = str.substring(0, char === "." ? i : i + 1)
+                break
+            }
+        }
+    }
+
+    return Number(str)
+}
