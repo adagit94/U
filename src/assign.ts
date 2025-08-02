0932beeb2c6cb119e5a7b5a6ace088dc8aff5be9
Adam Pelc <sikulin94@gmail.com>
2025-08-02 13:36:24 +0300

    .

--- src/assign.ts
+++ src/assign.ts
@@ -1,3 +1,5 @@
+import { isRecord } from "types.js";
+
 export function assign2<T extends Record<PropertyKey, unknown>, U extends Record<PropertyKey, unknown>>(
   o1: T,
   o2: U
@@ -17,7 +19,23 @@ export function assign4<
   T extends Record<PropertyKey, unknown>,
   U extends Record<PropertyKey, unknown>,
   V extends Record<PropertyKey, unknown>,
-  W extends Record<PropertyKey, unknown>,
+  W extends Record<PropertyKey, unknown>
 >(o1: T, o2: U, o3: V, o4: W): Omit<T, keyof (U | V | W)> & (Omit<U, keyof (V | W)> & (Omit<V, keyof W> & W)) {
   return Object.assign({}, o1, o2, o3, o4);
+}
+
+export function deepAssign<
+  T extends Record<PropertyKey, unknown>,
+  U extends Record<PropertyKey, unknown>
+>(o1: T, o2: U): RecursiveAssignment<T, U> {
+  const keys = new Set([...Object.keys(o1), ...Object.keys(o2)])
+  let obj = {}
+
+  for (const key of keys) {
+    if (key in o1 && key in o2) {
+      if (isRecord(o1) && isRecord(o2)) {
+        
+      }
+    }
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -5,19 +5,29 @@ export type Variadic<T, U extends unknown[] = []> = (...args: U) => T;
 export type ImmutableGetter<T> = () => Readonly<T>;
 
 export type ConversionRecursive<Obj extends object, ValueType> = {
-    [k in keyof Obj]: Obj[k] extends object ? ConversionRecursive<Obj[k], ValueType> : ValueType
-}
+  [k in keyof Obj]: Obj[k] extends object ? ConversionRecursive<Obj[k], ValueType> : ValueType;
+};
 
 export type UnionValues<Obj extends object, ValueType> = {
-    [k in keyof Obj]: Obj[k] | ValueType
-}
+  [k in keyof Obj]: Obj[k] | ValueType;
+};
 
 export type UnionValuesRecursive<Obj extends object, ValueType> = {
-    [k in keyof Obj]: Obj[k] extends object
-        ? ConversionRecursive<Obj[k], ValueType>
-        : Obj[k] | ValueType
-}
+  [k in keyof Obj]: Obj[k] extends object ? ConversionRecursive<Obj[k], ValueType> : Obj[k] | ValueType;
+};
 
 export type PartialRecursive<Obj extends object> = Partial<{
-    [k in keyof Obj]: Obj[k] extends object ? PartialRecursive<Obj[k]> : Obj[k]
-}>
+  [k in keyof Obj]: Obj[k] extends object ? PartialRecursive<Obj[k]> : Obj[k];
+}>;
+
+export type RecursiveAssignment<T extends Record<PropertyKey, unknown>, U extends Record<PropertyKey, unknown>> = {
+  [K in keyof Pick<T, keyof U>]: T[K] extends Record<PropertyKey, unknown>
+    ? U[K] extends Record<PropertyKey, unknown>
+      ? RecursiveAssignment<T[K], U[K]>
+      : U[K]
+    : U[K];
+} & Omit<T, keyof U> &
+  Omit<U, keyof T>;
+
+export const isRecord = (x: unknown): x is Record<PropertyKey, unknown> =>
+  typeof x === "object" && x !== null && !Array.isArray(x);
