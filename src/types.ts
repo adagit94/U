40d799223d64b9473b1e4db2b1ab09ebe88a85e2
Adam Pelc <sikulin94@gmail.com>
2025-08-20 19:30:19 +0200

    .

--- src/types.ts
+++ src/types.ts
@@ -29,5 +29,28 @@ export type RecursiveAssignment<T extends Record<PropertyKey, unknown>, U extend
 } & Omit<T, keyof U> &
   Omit<U, keyof T>;
 
+/**
+@description
+A utility type that converts specific properties (some or all) of first record to value types defined in second one.
+
+@example
+type Rec = RecordConversion<{ a: number; b: string }, { b: number }> // { a: number; b: number }
+*/
+export type RecordConversion<T extends Record<PropertyKey, unknown>, U extends { [K in keyof T]?: unknown }> = Omit<
+  T,
+  keyof U
+> &
+  U;
+
+/**
+@description
+A utility type that makes just specific properties of first record optional.
+
+@example
+type Rec = RecordOptionals<{ a: number; b: string }, "b"> // { a: number; b?: string }
+*/
+export type RecordOptionals<T extends Record<PropertyKey, unknown>, U extends keyof T> = Omit<T, U> &
+  Partial<Pick<T, U>>;
+
 export const isRecord = (x: unknown): x is Record<PropertyKey, unknown> =>
   typeof x === "object" && x !== null && !Array.isArray(x);
