5b9ceafa0ee8ec1ce6660df4e7f51215fbf6c1ed
Adam Pelc <sikulin94@gmail.com>
2025-11-17 06:34:33 +0100

    rename functions

--- src/filter.ts
+++ src/filter.ts
@@ -1,10 +1,10 @@
 import { isEqual, get } from "lodash";
 
-export const disjoint = <T>(a: T[], b: T[], comparator: (a: T, b: T) => boolean): T[] =>
+export const difference = <T>(a: T[], b: T[], comparator: (a: T, b: T) => boolean): T[] =>
   a.filter((item) => !b.some((item2) => comparator(item, item2)));
 
-export const disjointByPath = <T>(a: T[], b: T[], keyPath: string | string[]): T[] =>
-  disjoint(a, b, (item, item2) => get(item, keyPath) === get(item2, keyPath));
+export const differenceByPath = <T>(a: T[], b: T[], keyPath: string | string[]): T[] =>
+  difference(a, b, (item, item2) => get(item, keyPath) === get(item2, keyPath));
 
 export const intersection = <T>(a: T[], b: T[], comparator: (a: T, b: T) => boolean): T[] =>
   a.filter((item) => b.some((item2) => comparator(item, item2)));
