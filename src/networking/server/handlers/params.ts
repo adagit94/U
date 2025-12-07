47074892fec735d6f301997e7659a08efb5d569c
Adam Pelc <sikulin94@gmail.com>
2025-12-07 05:52:07 +0100

    common server handlers

---
+++ src/networking/server/handlers/params.ts
@@ -0,0 +1,71 @@
+export function handleReqParams<T extends z.ZodType>(
+  req: Request,
+  res: Response,
+  schemas: { path: T },
+): { path: z.infer<T> } | null;
+export function handleReqParams<T extends z.ZodType>(
+  req: Request,
+  res: Response,
+  schemas: { query: T },
+): { query: z.infer<T> } | null;
+export function handleReqParams<T extends z.ZodType, U extends z.ZodType>(
+  req: Request,
+  res: Response,
+  schemas: { path: T; query: U },
+): { path: z.infer<T>; query: z.infer<U> } | null;
+export function handleReqParams<T extends z.ZodType, U extends z.ZodType>(
+  req: Request,
+  res: Response,
+  schemas: { path: T } | { query: U } | { path: T; query: U },
+): { path: z.infer<T> } | { query: z.infer<U> } | { path: z.infer<T>; query: z.infer<U> } | null {
+  if ("path" in schemas && "query" in schemas) {
+    const pathParams = handleParams(req, res, "path", schemas.path);
+
+    if (pathParams === null) return null;
+
+    const queryParams = handleParams(req, res, "query", schemas.query);
+
+    if (queryParams === null) return null;
+
+    return { path: pathParams, query: queryParams };
+  }
+
+  if ("path" in schemas) {
+    const pathParams = handleParams(req, res, "path", schemas.path);
+
+    if (pathParams === null) return null;
+
+    return { path: pathParams };
+  }
+
+  if ("query" in schemas) {
+    const queryParams = handleParams(req, res, "query", schemas.query);
+
+    if (queryParams === null) return null;
+
+    return { query: queryParams };
+  }
+
+  return null;
+}
+
+const handleParams = <T extends "path" | "query", U extends z.ZodType>(
+  req: Request,
+  res: Response,
+  type: T,
+  schema: U,
+): z.infer<U> | null => {
+  const params = type === "path" ? req.params : req.query;
+  const parsingResult = schema.safeParse(params);
+
+  if (!parsingResult.success) {
+    res.status(400).send({
+      message: `${type[0].toUpperCase()}${type.substring(1)} params are invalid.`,
+      error: parsingResult.error.issues,
+    });
+
+    return null;
+  }
+
+  return parsingResult.data;
+};
